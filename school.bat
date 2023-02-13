6726819ade9d11157a09d2572e42bb5186a641da
Squawkykaka <100256401+Squawkykaka@users.noreply.github.com>
2023-02-14 11:21:31 +1300

    Changed paper download

--- school.bat
+++ school.bat
@@ -25,11 +25,13 @@ goto :start
 cls
 set /p name=What should the server be called?: 
 set /p ram=How much ram can the server use(Gigabytes)?: 
+set /p version=What is the minecraft cersion of the server(Answer like this example 1.19.4)?: 
 mkdir %name%
 cd %name%
-echo java -Xmx%RAM%g -Xms1g -jar paper.jar > start.bat
-echo eula=true > eula.txt
-curl https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/384/downloads/paper-1.18.2-384.jar --output paper.jar
+echo java -Xms250M -Xmx%ram%G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --noguii> start.bat
+echo eula=true>eula.txt
+rm paper.jar
+curl https://papermc.io/api/v1/paper/%version%/latest/download
 cls
 echo Done
 goto start
