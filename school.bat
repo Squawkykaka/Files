55370ff17efed0f4964637092319928f86313154
Squawkykaka <100256401+Squawkykaka@users.noreply.github.com>
2023-02-14 14:47:52 +1300

    Fixxed papermc download

--- school.bat
+++ school.bat
@@ -28,10 +28,10 @@ set /p ram=How much ram can the server use(Gigabytes)?:
 set /p version=What is the minecraft cersion of the server(Answer like this example 1.19.4)?: 
 mkdir %name%
 cd %name%
-echo java -Xms250M -Xmx%ram%G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --noguii> start.bat
+echo java -Xms250M -Xmx%ram%G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui> start.bat
 echo eula=true>eula.txt
 rm paper.jar
-curl https://papermc.io/api/v1/paper/%version%/latest/download
+curl https://papermc.io/api/v1/paper/%version%/latest/download --output paper.jar
 cls
 echo Done
 goto start
